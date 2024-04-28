40818b1bff5a8b26556ab89cc5285948705e1d31
GhostDevil274 <minhanhnguyentran274@gmail.com>
2024-04-28 11:02:35 +0700

    addDriver

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -44,11 +44,14 @@ const userListFunc = document.querySelector('.adminSection1__userList');
 
 const addDriver = document.querySelector('.adminSection1__addDriver');
 
+const driverListFunc = document.querySelector('.adminSection1__driverList');
+
 userListButton.addEventListener('click', () => {
-    
+    userListFunc.innerHTML = '';
     userListFunc.style.display = "block";
     addDriver.style.display = "none";
-
+    userListFunc.innerHTML = '';
+    driverListFunc.innerHTML ='';
 
     const userRef = collection(db, 'users');
     let userList = [];
@@ -68,7 +71,7 @@ userListButton.addEventListener('click', () => {
             tel.innerHTML = `<span>Số điện thoại: </span><span>${user.tel}</span>`;
             const email = document.createElement('li');
             email.innerHTML = `<span>Email: </span><span>${user.email}</span>`
-
+            
             let ulElement = document.createElement('ul');
             ulElement.classList.add('adminSection1__userInfo');
             ulElement.appendChild(username);
@@ -97,7 +100,7 @@ addDriverButton.addEventListener('click', () => {
     const car = document.querySelector('.adminSection1__driverCarTypeCar');
     const truck = document.querySelector('.adminSection1__driverCarTypeTruck');
     const container = document.querySelector('.adminSection1__driverCarTypeContainer');
-    
+
     const updateDisplay = () => {
         if (carType.value === 'Xe khách'){
             car.style.display = "flex";
@@ -115,18 +118,146 @@ addDriverButton.addEventListener('click', () => {
             container.style.display = "flex";
         }
     };
-
+    
     carType.addEventListener('change', updateDisplay);
+    updateDisplay();    
 
-    updateDisplay();
-
-    
 });
 
-driverListButton.addEventListener('click', () => {
-    
+const add__Driver_button = document.querySelector("#add__Driver");
+const adminSection1__driverName = document.querySelector("#adminSection1__driverName");
+const adminSection1__driverTel = document.querySelector("#adminSection1__driverTel");
+const adminSection1__driverCarType = document.querySelector("#adminSection1__driverCarType");
+const adminSection1__driverCarTypeCarSize = document.querySelector("#adminSection1__driverCarTypeCarSize");
+const adminSection1__driverCarTypeTruckWeight = document.querySelector("#adminSection1__driverCarTypeTruckWeight");
+const adminSection1__driverCarTypeContainerSize = document.querySelector("#adminSection1__driverCarTypeContainerSize");
+const adminSection1__driverLicense = document.querySelector("#adminSection1__driverLicense");
+
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = '';
+    for (var i = 0; i < 8; i++) {
+        var r = Math.floor(Math.random() * 10);
+        uuid += r; 
+    }
+    return uuid;
+}
+
+
+add__Driver_button.addEventListener('click', async (event) => {
+    event.preventDefault();
+    const driver_collection = collection(db, 'drivers');
+    const driver_Name = adminSection1__driverName.value;
+    const driver_ID = create_UUID();
+    const driver_Tel = adminSection1__driverTel.value;
+    const car_Type = adminSection1__driverCarType.value;
+    const driver_License = adminSection1__driverLicense.value;
+    let car_Size;
+    let truck_Weight;
+    let container_Size;
+
+    try {
+        await setDoc(doc(db, 'drivers', driver_ID), {
+            fullName: driver_Name,
+            tel: driver_Tel,
+            license: driver_License
+        });
+
+        let subCollectionRef = collection(db, `drivers/${driver_ID}/Vehicles`);
+
+        let vehicleData;
+
+        switch(car_Type) {
+            case "Xe khách":
+                car_Size = adminSection1__driverCarTypeCarSize.value;
+                vehicleData = { Type: car_Type, Size: car_Size };
+                break;
+            case "Xe tải":
+                truck_Weight = adminSection1__driverCarTypeTruckWeight.value;
+                vehicleData = { Type: car_Type, Weight: truck_Weight };
+                break;
+            case "Xe container":
+                container_Size = adminSection1__driverCarTypeContainerSize.value;
+                vehicleData = { Type: car_Type, Size: container_Size };
+                break;
+            default:
+                throw new Error("Invalid car type");
+        }
+
+        await addDoc(subCollectionRef, vehicleData);
+        alert(`Thêm tài xế thành công, mã định danh của tài xế là ${driver_ID}`);
+        console.log('Vehicle data added successfully');
+        
+        window.location.reload(); 
+
+    } catch (error) {
+        console.error("Error adding driver or vehicle data: ", error);
+    }
 });
+
+
+driverListButton.addEventListener('click', async () => {
+    driverListFunc.innerHTML = '';
+    driverListFunc.style.display = "block";
+    addDriver.style.display = "none";
+    driverListFunc.innerHTML = '';
+
+    try {
+        const driverRef = collection(db, 'drivers');
+        const driverSnapshot = await getDocs(driverRef);
+        const driverList = driverSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+        let ulDriverList = [];
+
+        for (const driver of driverList) {
+            const fullName = document.createElement('li');
+            fullName.innerHTML = `<span>Họ và tên: </span><span>${driver.fullName}</span>`;
+            const tel = document.createElement('li');
+            tel.innerHTML = `<span>Số điện thoại: </span><span>${driver.tel}</span>`;
+            
+
+            let ulElement = document.createElement('ul');
+            ulElement.classList.add('adminSection1__driverInfo');
+            ulElement.appendChild(fullName);
+            ulElement.appendChild(tel);
+
+            const vehiclesRef = collection(db, `drivers/${driver.id}/Vehicles`);
+            const vehiclesSnapshot = await getDocs(vehiclesRef);
+            const vehicleList = vehiclesSnapshot.docs.map(doc => doc.data());
+
+            if (vehicleList.length > 0) {
+                for (const vehicle of vehicleList) {
+                    const vehicleType = document.createElement('li');
+                    vehicleType.innerHTML = `<span>Loại xe: </span><span>${vehicle.Type}</span>`;
+                    const vehicleWeight = document.createElement('li');
+                    vehicleWeight.innerHTML = `<span>Kích thước: </span><span>${vehicle.Size}</span>`;
+
+                    let ulVehicleElement = document.createElement('ul');
+                    ulVehicleElement.classList.add('adminSection1__driverInfo');
+                    ulVehicleElement.appendChild(vehicleType);
+                    ulVehicleElement.appendChild(vehicleWeight);
+
+                    ulElement.appendChild(ulVehicleElement);
+                }
+            }
+            const license = document.createElement('li');
+            license.innerHTML = `<span>Giấy phép lái xe: </span><span>${driver.license}</span>`;
+            ulElement.appendChild(license);
+            
+            ulDriverList.push(ulElement);
+        }
+
+        const driverListDiv = document.querySelector('.adminSection1__driverList');
+        ulDriverList.forEach((element) => {
+            driverListDiv.appendChild(element);
+        });
+    } catch (error) {
+        console.error("Error fetching driver or vehicle data: ", error);
+    }
+});
+
+
 
 carListButton.addEventListener('click', () => {
-
+    
 });
